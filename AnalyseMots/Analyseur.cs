98292bfa59a244ed65605a0700059c25ec5cd961
MelinaDCB <89150123+MelinaDCB@users.noreply.github.com>
2022-06-05 12:31:15 +0200

    WordAnalyser completed :)

---
+++ AnalyseMots/Analyseur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyseMots
+{
+    internal class Analyseur
+    {
+        public static void CompterLettres(ref string chosenWord, out int nombreVoyelle, out int nombreConsonne)
+        {
+            nombreConsonne = 0;
+            nombreVoyelle = 0;
+            for (int index = 0; index < chosenWord.Length ; index++)
+            {
+                if (chosenWord[index] == 'a' || chosenWord[index] == 'e' || chosenWord[index] == 'i' || chosenWord[index] == 'o' || chosenWord[index] == 'u' || chosenWord[index] == 'y')
+                {
+                    nombreVoyelle++;
+                }
+                else
+                {
+                    nombreConsonne++;
+                }
+            }
+
+        }
+    }
+}
---
+++ AnalyseMots/Program.cs
@@ -0,0 +1,7 @@
+﻿using AnalyseMots;
+
+Console.WriteLine("Choisissez le mot à analyser :");
+string word = Console.ReadLine();
+int nbVoyelle, nbConsonne;
+Analyseur.CompterLettres(ref word, out nbVoyelle, out nbConsonne);
+Console.WriteLine($"Le mot est donc composé de {word.Length} lettres dont {nbVoyelle} voyelles et {nbConsonne} consonnes.");
