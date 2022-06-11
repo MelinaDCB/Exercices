9127c3825ca23f42875fb907a46e4130ab7dab20
MelinaDCB <89150123+MelinaDCB@users.noreply.github.com>
2022-06-12 00:15:52 +0200

    Distrib café exercice fini

---
+++ DistribBoisson/Distributeur.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistribBoisson
+{
+    public class Distributeur
+    {
+        private double _solde;
+
+        public double Solde
+        {
+            get { return _solde; }
+            set { _solde = value; }
+        }
+        public Distributeur(double solde)
+        {
+            Solde = solde;
+        }
+
+        public string ServirBoisson(int customerChoice, int Sucre)
+        {
+            if (Sucre > 10)
+                Sucre = 10;
+                if (Solde >= 0.80 && customerChoice == 1)
+                {
+                    Solde -= 0.80;
+                    return $"Café avec {Sucre}g de sucre.";
+                }
+                else if (Solde >= 0.75 && customerChoice == 2)
+                {
+                    Solde -= 0.75;
+                    return $"Chocolat avec {Sucre}g de sucre.";
+                }
+                else if (Solde >= 1.00 && customerChoice == 3)
+                {
+                    Sucre = 0;
+                    Solde -= 1.00;
+                    return $"Soupe avec {Sucre}g de sucre.";
+                }
+                else
+                {
+                    return "Choix et/ou solde insuffisant.";
+                }
+        }
+    }
+
+}
---
+++ DistribBoisson/Program.cs
@@ -0,0 +1,7 @@
+﻿using DistribBoisson;
+
+Distributeur distri1 = new Distributeur(2.00);
+
+Console.WriteLine(distri1.ServirBoisson(3, 5));
+Console.WriteLine(distri1.ServirBoisson(1, 12));
+Console.WriteLine(distri1.ServirBoisson(2, 7));
